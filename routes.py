47d177e7d9dbc460884b2c520df7351a6ef52ddc
Jake Marsden <100683486+jakemarsdenn@users.noreply.github.com>
2024-05-12 19:32:59 -0700

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, session, redirect
+
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+session = {}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', session=session)
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html', session=session)
+
+
