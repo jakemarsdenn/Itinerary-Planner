f71d667e5760231e09c2cf22767045b0d36229bc
Jake <jmar7284@uni.sydney.edu.au>
2024-05-12 23:25:19 -0700

    added local date_time to index page

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     animateText();
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    displayLocalDate();
+});
+
 function animateText() {
     const text = document.getElementById("title");
     const letters = text.textContent.split("");
@@ -9,8 +13,19 @@ function animateText() {
     letters.forEach((letter, index) => {
         const span = document.createElement("span");
         span.textContent = letter;
-        span.style.animationDelay = `${index * 100}ms`; // Delay each letter's animation
-        span.classList.add("loading"); // Apply the loading animation class
+        span.style.animationDelay = `${index * 100}ms`;
+        span.classList.add("loading");
         text.appendChild(span);
     });
+}
+
+function displayLocalDate() {
+    const dateElement = document.getElementById("date");
+    const currentDate = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const date = currentDate.toLocaleDateString(undefined, options);
+    const parts = date.split(' ');
+    parts[2] += ',';
+    const formattedDate = parts.join(' ');
+    dateElement.textContent = formattedDate;
 }
